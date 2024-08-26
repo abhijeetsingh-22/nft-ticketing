c3718a7e7cb70cc0ee4b6e63f0a6ee89562e9601
Rohit Gupta <81370462+rohitt-gupta@users.noreply.github.com>
2024-08-26 19:00:51 +0530

    Add: dashboard dummy data

--- components/admin-panel/content-layout.tsx
+++ components/admin-panel/content-layout.tsx
@@ -9,9 +9,9 @@ interface ContentLayoutProps {
 
 export function ContentLayout({ title, children, className }: ContentLayoutProps) {
   return (
-    <div>
+    <div className={className}>
       <Navbar title={title} />
-      <div className={cn("container pt-8 pb-8 px-4 sm:px-8", className)}>{children}</div>
+      <div className={cn("container pt-8 pb-8 px-4 sm:px-8")}>{children}</div>
     </div>
   );
 }
