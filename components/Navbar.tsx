7eb58bacc93d17823869110e618c9bfaf786ba99
Abhijeet Singh <asingh9829@gmail.com>
2024-08-26 22:30:12 +0530

    always show connect wallet button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,45 +1,51 @@
-
-import Link from 'next/link'
-import MaxWidthWrapper from './MaxWidthWrapper'
-import { buttonVariants } from './ui/button'
-import { ArrowRight, Github } from 'lucide-react'
-import MobileNav from './MobileNav'
-import SignOutButton from './auth/SignOutButton'
-import { auth } from '@/auth'
-import ConnectWalletButton from '@/components/ConnectWalletButton'
-import Image from 'next/image'
+import Link from "next/link"
+import MaxWidthWrapper from "./MaxWidthWrapper"
+import {buttonVariants} from "./ui/button"
+import {ArrowRight, Github} from "lucide-react"
+import MobileNav from "./MobileNav"
+import SignOutButton from "./auth/SignOutButton"
+import {auth} from "@/auth"
+import ConnectWalletButton from "@/components/ConnectWalletButton"
+import Image from "next/image"
 
 const Navbar = async () => {
-  const session = await auth()
+	const session = await auth()
 
-  const isAuth = session?.user ? true : false
+	const isAuth = session?.user ? true : false
 
+	return (
+		<nav className="top-0 z-30 sticky inset-x-0 border-gray-200 bg-white/75 backdrop-blur-lg border-b w-full h-14 transition-all">
+			<MaxWidthWrapper>
+				<div className="flex justify-between items-center border-zinc-200 border-b h-14">
+					<Link
+						href="/"
+						className="z-40 flex font-semibold items-center gap-2"
+					>
+						<Image
+							src="/logos/icon.png"
+							alt="MintTix"
+							width={30}
+							height={30}
+						/>
+						<span>MintTix</span>
+					</Link>
 
-  return (
-    <nav className='top-0 z-30 sticky inset-x-0 border-gray-200 bg-white/75 backdrop-blur-lg border-b w-full h-14 transition-all'>
-      <MaxWidthWrapper>
-        <div className='flex justify-between items-center border-zinc-200 border-b h-14'>
-          <Link
-            href='/'
-            className='z-40 flex font-semibold items-center gap-2'>
-              <Image src="/logos/icon.png" alt="MintTix" width={30} height={30} />
-            <span>MintTix</span>
-          </Link>
+					<MobileNav isAuth={isAuth} />
 
-          <MobileNav isAuth={isAuth} />
+					<div className="sm:flex items-center space-x-4 hidden">
+						<Link
+							href="/events"
+							className={buttonVariants({
+								variant: "ghost",
+								size: "sm",
+							})}
+						>
+							All Events
+						</Link>
 
-          <div className='sm:flex items-center space-x-4 hidden'>
-            <Link
-              href='/events'
-              className={buttonVariants({
-                variant: 'ghost',
-                size: 'sm',
-              })}>
-              All Events
-            </Link>
-            {!isAuth ? (
-              <>
-                {/* <Link
+						{!isAuth ? (
+							<>
+								{/* <Link
                   target='_blank'
                   href='https://github.com/abhijeetsingh-22/nft-ticketing'
                   className={buttonVariants({
@@ -48,7 +54,7 @@ const Navbar = async () => {
                   })}>
                   <Github className='w-5 h-5' />
                 </Link> */}
-                {/* <Link
+								{/* <Link
                   href='/login'
                   className={buttonVariants({
                     variant: 'ghost',
@@ -56,37 +62,44 @@ const Navbar = async () => {
                   })}>
                   Sign in
                 </Link> */}
-                <ConnectWalletButton />
-                <Link
-                  href='/signup'
-                  className={buttonVariants({
-                    size: 'sm',
-                  })}>
-                  Get started{' '}
-                  <ArrowRight className='ml-1.5 w-5 h-5' />
-                </Link>
-              </>
-            ) : (
-              <>
-                {
-                  <Link
-                    href={`/${session?.user?.id}/events`}
-                    className={buttonVariants({
-                      variant: 'ghost',
-                      size: 'sm',
-                    })}>
-                    My Events
-                  </Link>}
-                <Link className='cursor-pointer' href="/profile">{session?.user?.email}</Link>
-                <SignOutButton />
-              </>
-            )}
-
-          </div>
-        </div>
-      </MaxWidthWrapper>
-    </nav>
-  )
+								<ConnectWalletButton />
+								<Link
+									href="/signup"
+									className={buttonVariants({
+										size: "sm",
+									})}
+								>
+									Get started <ArrowRight className="ml-1.5 w-5 h-5" />
+								</Link>
+							</>
+						) : (
+							<>
+								{
+									<Link
+										href={`/${session?.user?.id}/events`}
+										className={buttonVariants({
+											variant: "ghost",
+											size: "sm",
+										})}
+									>
+										My Events
+									</Link>
+								}
+								<Link
+									className="cursor-pointer"
+									href="/profile"
+								>
+									{session?.user?.email}
+								</Link>
+								<ConnectWalletButton />
+								<SignOutButton />
+							</>
+						)}
+					</div>
+				</div>
+			</MaxWidthWrapper>
+		</nav>
+	)
 }
 
 export default Navbar
