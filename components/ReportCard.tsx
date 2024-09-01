012795573fb87ce4dff96f15f0b481b5c38a3aba
lokeshjha <lokesh1jha@gmai.com>
2024-09-01 10:38:13 +0530

    report issue working

--- components/ReportCard.tsx
+++ components/ReportCard.tsx
@@ -1,6 +1,6 @@
 "use client";
 import * as React from "react";
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import {
@@ -26,19 +26,20 @@ import { buildMailBodyForReportIssue } from "@/lib/email/emailBody";
 
 // Define the validation schema using Zod
 const reportSchema = z.object({
-	area: z.string(),
-	security: z.string().optional(),
+	area: z.string().min(1, "Area is required"),
+	security: z.string().min(1, "Security is required"),
 	subject: z.string().min(1, "Subject is required"),
 	description: z.string().min(1, "Description is required"),
 });
-
 
 type ReportFormData = z.infer<typeof reportSchema>;
 
 export default function ReportCard() {
 	const {
+		control,
 		register,
 		handleSubmit,
+		reset,
 		formState: { errors },
 	} = useForm<ReportFormData>({
 		resolver: zodResolver(reportSchema),
@@ -46,133 +47,140 @@ export default function ReportCard() {
 
 	const onSubmit = async (data: ReportFormData) => {
 		console.log(data);
-		let to = process.env.NEXT_PUBLIC_ADMIN_EMAIL
+		let to = process.env.NEXT_PUBLIC_ADMIN_EMAIL;
 		if (!to) {
-			toast.error("Failed to resgister your issue. Please try again");
-			return
+			toast.error("Failed to register your issue. Please try again");
+			return;
 		}
-		
-		// let mailBody: string = await buildMailBodyForReportIssue(data)
-		
-		// let response = await sendEmailUsingNodemailer(to, "Report Issue", mailBody)
+		let mailBody: string = await buildMailBodyForReportIssue(data);
 
-		// if (response) {
-		// 	toast.success("Issue reported successfully")
-		// } else {
-		// 	toast.error("Failed to resgister your issue. Please try again")
-		// }
+		let response = await sendEmailUsingNodemailer(to, "Report Issue", mailBody);
+
+		if (response) {
+			toast.success("Issue reported successfully");
+			reset(); // Clear all values after successful submission
+		} else {
+			toast.error("Failed to register your issue. Please try again");
+		}
 	};
 
 	return (
-		<Card className='bg-white dark:bg-gray-800 shadow-lg w-[400px]'>
+		<Card className="bg-white dark:bg-gray-800 shadow-lg w-[400px]">
 			<CardHeader>
-				<CardTitle className='font-semibold text-2xl text-black dark:text-white'>
+				<CardTitle className="font-semibold text-2xl text-black dark:text-white">
 					Report an issue
 				</CardTitle>
-				<p className='text-black text-sm dark:text-white'>
+				<p className="text-black text-sm dark:text-white">
 					What area are you having problems with?
 				</p>
 			</CardHeader>
 			<form onSubmit={handleSubmit(onSubmit)}>
-				<CardContent className='space-y-4'>
-					<div className='space-y-2'>
+				<CardContent className="space-y-4">
+					<div className="space-y-2">
 						<label
-							htmlFor='area'
-							className='font-medium text-black text-sm dark:text-white'
+							htmlFor="area"
+							className="font-medium text-black text-sm dark:text-white"
 						>
 							Area
 						</label>
-						<Select {...register("area")}>
-							<SelectTrigger
-								id='area'
-								className='border-gray-300 dark:border-gray-600 focus:ring-gray-500 dark:focus:ring-gray-400'
-							>
-								<SelectValue placeholder='Select area' />
-							</SelectTrigger>
-							<SelectContent>
-								<SelectItem value='billing'>Billing</SelectItem>
-								<SelectItem value='technical'>Technical</SelectItem>
-								<SelectItem value='account'>Account</SelectItem>
-							</SelectContent>
-						</Select>
+						<Controller
+							name="area"
+							control={control}
+							render={({ field }) => (
+								<Select onValueChange={field.onChange} value={field.value}>
+									<SelectTrigger
+										id="area"
+										className="border-gray-300 dark:border-gray-600 focus:ring-gray-500 dark:focus:ring-gray-400"
+									>
+										<SelectValue placeholder="Select area" />
+									</SelectTrigger>
+									<SelectContent>
+										<SelectItem value="billing">Billing</SelectItem>
+										<SelectItem value="technical">Technical</SelectItem>
+										<SelectItem value="account">Account</SelectItem>
+									</SelectContent>
+								</Select>
+							)}
+						/>
 						{errors.area && (
-							<p className='text-red-500'>{errors.area.message as string}</p>
+							<p className="text-red-500">{errors.area.message}</p>
 						)}
 					</div>
-					<div className='space-y-2'>
+					<div className="space-y-2">
 						<label
-							htmlFor='security'
-							className='font-medium text-black text-sm dark:text-white'
+							htmlFor="security"
+							className="font-medium text-black text-sm dark:text-white"
 						>
 							Security Level
 						</label>
-						<Select {...register("security")}>
-							<SelectTrigger
-								id='security'
-								className='border-gray-300 dark:border-gray-600 focus:ring-gray-500 dark:focus:ring-gray-400'
-							>
-								<SelectValue placeholder='Select security level' />
-							</SelectTrigger>
-							<SelectContent>
-								<SelectItem value='1'>Severity 1</SelectItem>
-								<SelectItem value='2'>Severity 2</SelectItem>
-								<SelectItem value='3'>Severity 3</SelectItem>
-							</SelectContent>
-						</Select>
+						<Controller
+							name="security"
+							control={control}
+							render={({ field }) => (
+								<Select onValueChange={field.onChange} value={field.value}>
+									<SelectTrigger
+										id="security"
+										className="border-gray-300 dark:border-gray-600 focus:ring-gray-500 dark:focus:ring-gray-400"
+									>
+										<SelectValue placeholder="Select security level" />
+									</SelectTrigger>
+									<SelectContent>
+										<SelectItem value="Severity 1">Severity 1</SelectItem>
+										<SelectItem value="Severity 2">Severity 2</SelectItem>
+										<SelectItem value="Severity 3">Severity 3</SelectItem>
+									</SelectContent>
+								</Select>
+							)}
+						/>
 						{errors.security && (
-							<p className='text-red-500'>
-								{errors.security.message as string}
-							</p>
+							<p className="text-red-500">{errors.security.message}</p>
 						)}
 					</div>
-					<div className='space-y-2'>
+					<div className="space-y-2">
 						<label
-							htmlFor='subject'
-							className='font-medium text-black text-sm dark:text-white'
+							htmlFor="subject"
+							className="font-medium text-black text-sm dark:text-white"
 						>
 							Subject
 						</label>
 						<Input
-							id='subject'
-							placeholder='I need help with...'
-							className='border-gray-300 dark:border-gray-600 focus:ring-gray-500 dark:focus:ring-gray-400'
+							id="subject"
+							placeholder="I need help with..."
+							className="border-gray-300 dark:border-gray-600 focus:ring-gray-500 dark:focus:ring-gray-400"
 							{...register("subject")}
 						/>
 						{errors.subject && (
-							<p className='text-red-500'>{errors.subject.message as string}</p>
+							<p className="text-red-500">{errors.subject.message}</p>
 						)}
 					</div>
-					<div className='space-y-2'>
+					<div className="space-y-2">
 						<label
-							htmlFor='description'
-							className='font-medium text-black text-sm dark:text-white'
+							htmlFor="description"
+							className="font-medium text-black text-sm dark:text-white"
 						>
 							Description
 						</label>
 						<Textarea
-							id='description'
-							placeholder='Please include all information relevant to your issue.'
-							className='border-gray-300 dark:border-gray-600 focus:ring-gray-500 dark:focus:ring-gray-400 min-h-[100px]'
+							id="description"
+							placeholder="Please include all information relevant to your issue."
+							className="border-gray-300 dark:border-gray-600 focus:ring-gray-500 dark:focus:ring-gray-400 min-h-[100px]"
 							{...register("description")}
 						/>
 						{errors.description && (
-							<p className='text-red-500'>
-								{errors.description.message as string}
-							</p>
+							<p className="text-red-500">{errors.description.message}</p>
 						)}
 					</div>
 				</CardContent>
-				<CardFooter className='flex justify-between'>
+				<CardFooter className="flex justify-between">
 					<Button
-						variant='outline'
-						className='border-gray-300 dark:border-gray-600 hover:bg-gray-50 dark:hover:bg-gray-900 text-black dark:text-white'
+						variant="outline"
+						className="border-gray-300 dark:border-gray-600 hover:bg-gray-50 dark:hover:bg-gray-900 text-black dark:text-white"
 					>
 						Cancel
 					</Button>
 					<Button
-						type='submit'
-						className='bg-primary hover:bg-primary-dark dark:bg-primary-dark text-white'
-						onClick={handleSubmit(onSubmit)}
+						type="submit"
+						className="bg-primary hover:bg-primary-dark dark:bg-primary-dark text-white"
 					>
 						Submit
 					</Button>
